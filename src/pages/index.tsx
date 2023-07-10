56ba779fb15fa4eb67100abde7f719d06a7bb0db
Alvin <alvinmartindwrk@gmail.com>
2023-07-10 19:59:59 +0700

    setup apollo client

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,62 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from 'next/head';
+import { Inter } from 'next/font/google';
+import { gql, useQuery } from '@apollo/client';
+import { useState } from 'react';
+import Image from 'next/image';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
+
+const GET_ANIME = gql`
+  query ($id: Int, $page: Int, $perPage: Int) {
+    Page(page: $page, perPage: $perPage) {
+      pageInfo {
+        total
+        currentPage
+        lastPage
+        hasNextPage
+        perPage
+      }
+      # Define which variables will be used in the query (id)
+      media(id: $id, type: ANIME) {
+        # Insert our variables into the query arguments (id) (type: ANIME is hard-coded in the query)
+        id
+        title {
+          english
+        }
+        coverImage {
+          large
+        }
+      }
+    }
+  }
+`;
 
 export default function Home() {
+  const [page, setPage] = useState(1);
+
+  const anime = useQuery(GET_ANIME, {
+    variables: {
+      page,
+      perPage: 10,
+    },
+  });
+
+  function handlePagination(type: 'PREV' | 'NEXT') {
+    if (type === 'PREV' && page > 1) {
+      setPage(page - 1);
+    } else if (type === 'NEXT' && anime.data.Page.pageInfo.hasNextPage) {
+      setPage(page + 1);
+    }
+  }
+
+  if (anime.loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (anime.error) {
+    return <p>Error!!</p>;
+  }
+
   return (
     <>
       <Head>
@@ -14,101 +65,16 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+      <main className={`${inter.className}`}>
+        {anime.data.Page.media.map(((m: any) => (
+          <div key={m.id}>
+            <Image src={m.coverImage.large} alt={m.title.english} width={100} height={100} />
+            <p>{m.title.english}</p>
           </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        )))}
+        <button onClick={() => handlePagination('PREV')}>prev</button>
+        <button onClick={() => handlePagination('NEXT')}>next</button>
       </main>
     </>
-  )
+  );
 }
